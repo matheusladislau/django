8b0ed3b490ac5fab87e5cd24b7c5c5a4cdad5edd
matheusladislau <matheusladislauribeiro@gmail.com>
2019-03-10 13:50:53 -0300

    creating Produto table in model

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Produto(models.Model):
+    nome=models.CharField(max_length=100)
+    dt_criacao=models.DateTimeField()
+    descricao=models.CharField(max_length=1000)
+    valor=models.DecimalField(max_digits=7,decimal_places=2)
+
+    # ao referir ao plural
+    class Meta:
+        verbose_name_plural='Produtos'
+
+    # ao selecionar, retorna
+    def __str__(self):
+        return self.nome
